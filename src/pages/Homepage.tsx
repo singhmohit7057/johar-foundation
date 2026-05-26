a66049247a234a78c3b6fa023edafa1030876038
Mohit Singh <singhmohit7057@gmail.com>
2026-05-26 09:53:07 +0530

    FIX

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { theme } from '../theme/styles';
 import { SEO } from '../shared/seo';
